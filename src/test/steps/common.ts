01bb4f0b9903b5eb2be0f7d6829a1689a5f158ca
Jason Paige <j@jasonpaige.co.uk>
2022-12-06 10:51:44 +0000

    func tests

--- src/test/steps/common.ts
+++ src/test/steps/common.ts
@@ -1,5 +1,3 @@
-import fs from 'fs';
-
 import { config as testConfig } from '../config';
 
 const { I } = inject();
@@ -11,25 +9,8 @@ export const iAmOnPage = (text: string): void => {
   }
   I.amOnPage(url.toString());
 };
-export const loadedFile = (path: string): string => {
-  return fs.readFileSync(path, 'utf8');
-};
-
-export const sourceRepo = (repo: string): string => {
-  return repo;
-};
-
-export const postToArdoq = (url: string): string => {
-  return url;
-};
 
 Given('I go to {string}', iAmOnPage);
-
-Given("A payload of '{string}'", loadedFile);
-
-Given('My repo {string}', sourceRepo);
-
-When("I POST to '{string}'", postToArdoq);
 
 Then('the page URL should be {string}', (url: string) => {
   I.waitInUrl(url);
