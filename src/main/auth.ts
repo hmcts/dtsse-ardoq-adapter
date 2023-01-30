df541f8d3eb496b3dfcbe5de4fde1e248b3a2fbf
Jason Paige <j@jasonpaige.co.uk>
2023-01-30 14:31:52 +0000

    debug

--- src/main/auth.ts
+++ src/main/auth.ts
@@ -1,6 +1,8 @@
 import config from 'config';
 import { NextFunction, Request, Response } from 'express';
 
+const { Logger } = require('@hmcts/nodejs-logging');
+
 export function isAuthorised(req: Request, res: Response, next: NextFunction): void {
   if (
     req.headers.authorization === 'Bearer ' + config.get('serverApiKey.primary') ||
@@ -8,6 +10,9 @@ export function isAuthorised(req: Request, res: Response, next: NextFunction): v
   ) {
     next();
   } else {
+    const logger = Logger.getLogger('app');
+    const header = req.headers.authorization ?? 'undefined';
+    logger.error('Unauthorised request. Beader ends with: ' + header.slice(-9));
     res.status(403).json({ error: 'Not Authorized' });
     throw new Error('Not Authorized');
   }
