fffdd7d9995167cd30e8d465cd40be56f246488d
Jason Paige <j@jasonpaige.co.uk>
2023-01-31 11:11:19 +0000

    more debug

--- src/main/auth.ts
+++ src/main/auth.ts
@@ -6,14 +6,17 @@ const { Logger } = require('@hmcts/nodejs-logging');
 export function isAuthorised(req: Request, res: Response, next: NextFunction): void {
   const header = req.headers.authorization ?? 'undefined';
   const logger = Logger.getLogger('auth');
+  const expected: string = config.get('serverApiKey.primary') ?? 'undefined';
   logger.info('Bearer ends with: ' + header.slice(-9));
+  logger.info('Bearer is ' + header.length + ' characters long');
+  logger.info('Expected to end with: ' + expected.slice(-9));
+  logger.info('Bearer is ' + expected.length + ' characters long');
   if (
     req.headers.authorization === 'Bearer ' + config.get('serverApiKey.primary') ||
     req.headers.authorization === 'Bearer ' + config.get('serverApiKey.secondary')
   ) {
     next();
   } else {
-    logger.info('Unauthorised request. Bearer ends with: ' + header.slice(-9));
     res.status(403).json({ error: 'Not Authorized' });
     throw new Error('Not Authorized');
   }
