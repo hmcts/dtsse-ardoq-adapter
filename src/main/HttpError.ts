67ede29852a1c47b2ad22404978d37003710373c
Jason Paige <j@jasonpaige.co.uk>
2023-04-19 16:47:09 +0100

    make http error list and status readonly

--- src/main/HttpError.ts
+++ src/main/HttpError.ts
@@ -1,6 +1,6 @@
 export class HTTPError extends Error {
-  status: number;
-  errors: unknown[]; // this is to play nicely with openapi-validator ValidationError
+  readonly status: number;
+  readonly errors: unknown[]; // this is to play nicely with openapi-validator ValidationError
 
   constructor(message: string, status: number, errors?: unknown[]) {
     super(message);
