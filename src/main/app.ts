a9810ebcba397ef0b91e3d0b6b88841693d83c2b
Jason Paige <j@jasonpaige.co.uk>
2023-05-19 14:11:42 +0100

    log errors in prod temporarily

--- src/main/app.ts
+++ src/main/app.ts
@@ -64,7 +64,7 @@ app.use((req, res) => {
 
 app.use((err: HTTPError | ValidationError, req: express.Request, res: express.Response, next: express.NextFunction) => {
   logger.error(err.message);
-  logger.debug(JSON.stringify(err));
+  logger.error(JSON.stringify(err));
   res.status(err.status || 500).json({
     message: err.message,
     errors: err.errors,
