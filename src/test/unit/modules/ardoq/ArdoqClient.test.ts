f54bfaeb49f42ef4ae6ed14cea28a35891473cda
Jason Paige <j@jasonpaige.co.uk>
2023-07-13 16:32:39 +0100

    remove commented out code

--- src/test/unit/modules/ardoq/ArdoqClient.test.ts
+++ src/test/unit/modules/ardoq/ArdoqClient.test.ts
@@ -230,12 +230,6 @@ describe('ArdoqClient', () => {
     expect(r).toEqual('1234');
   });
 
-  // it('getComponentIdIfExists known', async () => {
-  //   const client = new ArdoqClient(mockedAxios, cache);
-  //   const r = await client.getComponentIdIfExists('known-but-not-cached');
-  //   expect(r).toEqual('91011');
-  // });
-
   it('getComponentIdIfExists unknown', async () => {
     const client = new ArdoqClient(mockedAxios, cache);
     const r = await client.getComponentIdIfExists('unknown');
